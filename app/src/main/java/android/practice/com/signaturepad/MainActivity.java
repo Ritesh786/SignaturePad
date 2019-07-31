36f8b76c7f7748ec797d152f463118aa3e675bab
Nurul <nurul@avsstech.com>
2019-07-31 12:00:21 +0530

    hjgjhggb

---
+++ app/src/main/java/android/practice/com/signaturepad/MainActivity.java
@@ -0,0 +1,85 @@
+package android.practice.com.signaturepad;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
+
+public class MainActivity extends AppCompatActivity {
+
+     ImageView mgetsignbtn;
+     public static final int SIGNATURE_CONSTANT = 2;
+     Activity activity;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mgetsignbtn = findViewById(R.id.getsign_btn);
+        activity = MainActivity.this;
+        mgetsignbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Dexter.withActivity(activity)
+                        .withPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                        .withListener(new PermissionListener() {
+                            @Override
+                            public void
+                            onPermissionGranted(PermissionGrantedResponse response) {
+                                Intent intent = new Intent(MainActivity.this, SignActivity.class);
+                                activity.startActivityForResult(intent, SIGNATURE_CONSTANT);
+                            }
+                            @Override
+                            public void
+                            onPermissionDenied(PermissionDeniedResponse response) {
+                                Toast.makeText(activity, "Pls Give Permission", Toast.LENGTH_SHORT).show();
+                            }
+                            @Override
+                            public void onPermissionRationaleShouldBeShown(PermissionRequest permission, PermissionToken token) {
+                                token.continuePermissionRequest();
+                            }
+                        }).check();
+            }
+        });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Bitmap originalBitmap = null;
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inPreferredConfig = Bitmap.Config.ARGB_8888;
+        options.inJustDecodeBounds = true;
+        options.inScaled = false;
+        String path;
+
+        switch (requestCode) {
+            case SIGNATURE_CONSTANT:
+                if (resultCode != Activity.RESULT_OK) return;
+                path = data.getStringExtra("path");
+                BitmapFactory.decodeFile(path, options);
+                options.inSampleSize = 0;//ImageUtil.inSampleSize(options, img.getControl().getW().intValue(), img.getControl().getH().intValue());
+                options.inJustDecodeBounds = false;
+                originalBitmap = BitmapFactory.decodeFile(path, options);
+                mgetsignbtn.setImageBitmap(originalBitmap);
+                break;
+        }
+    }
+}
---
+++ app/src/main/java/android/practice/com/signaturepad/SignActivity.java
@@ -0,0 +1,77 @@
+package android.practice.com.signaturepad;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Environment;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import com.azeesoft.lib.colorpicker.ColorPickerDialog;
+
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+public class SignActivity extends AppCompatActivity {
+
+    CanvasView customCanvas;
+    String DIRECTORY = Environment.getExternalStorageDirectory().getPath() + "/UserSignature/";
+    String pic_name = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault()).format(new Date());
+    String StoredPath = DIRECTORY + pic_name + ".png";
+    File file;
+    ColorPickerDialog colorPickerDialog;
+    TextView mcolorshow;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign);
+
+        mcolorshow = findViewById(R.id.colorshow_txt);
+        customCanvas = (CanvasView) findViewById(R.id.signature_canvas);
+        file = new File(DIRECTORY);
+        if (!file.exists()) {
+            file.mkdir();
+        }
+
+
+        mcolorshow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                colorPickerDialog = ColorPickerDialog.createColorPickerDialog(SignActivity.this);
+                colorPickerDialog.show();
+                colorPickerDialog.hideHexaDecimalValue();
+
+                colorPickerDialog.setOnColorPickedListener(new ColorPickerDialog.OnColorPickedListener() {
+                    @Override
+                    public void onColorPicked(int color, String hexVal) {
+                        mcolorshow.setBackgroundColor(color);
+                        customCanvas.setcolor(color);
+                    }
+                });
+            }
+        });
+
+    }
+    public void clearCanvas(View v) {
+        customCanvas.clear();
+    }
+
+
+    public void save(View v) {
+        customCanvas.save(customCanvas,StoredPath);
+        Intent _intent_Result = new Intent();
+        _intent_Result.putExtra("path",StoredPath);
+        setResult(RESULT_OK, _intent_Result);
+        finish();
+    }
+
+
+}
